50fe43c27e4cc20f12a2898ef780dfbc6b72ce25
jason <jason@e29b0fa8-351f-db11-9190-0011d8377dfc>
2008-09-15 19:23:14 +0000

    <rev> Adding missing file to repo
    git-svn-id: file:///export/repository/projects/pchdtv/libtuner@314 e29b0fa8-351f-db11-9190-0011d8377dfc

---
+++ tuner_device.cpp
@@ -0,0 +1,73 @@
+/*-
+ * Copyright 2007 Jason Harmening
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ */
+
+#include <sys/errno.h>
+#include "tuner_device.h"
+
+int tuner_device::write_array(const uint8_t *buffer, size_t elem_size, size_t total_size)
+{
+   int error = 0;
+   if ((total_size % elem_size) != 0)
+   {
+      return EINVAL;
+   }
+   for (size_t i = 0; i < total_size; i += elem_size)
+   {
+      if ((error = write(buffer + i, elem_size)))
+      {
+         return error;
+      }
+   }
+   return error;
+}
+
+int tuner_device::read_array(uint8_t *buffer, size_t elem_size, size_t total_size)
+{
+   int error = 0;
+   if ((total_size % elem_size) != 0)
+   {
+      return EINVAL;
+   }
+   for (size_t i = 0; i < total_size; i += elem_size)
+   {
+      if ((error = read(buffer + i, elem_size)))
+      {
+         return error;
+      }
+   }
+   return error;
+}
+
+int tuner_device::transact(const uint8_t *write_buffer, size_t write_size, uint8_t *read_buffer, size_t read_size)
+{
+   int error = write(write_buffer, write_size);
+   if (error)
+   {
+      return error;
+   }
+   return read(read_buffer, read_size);   
+}
